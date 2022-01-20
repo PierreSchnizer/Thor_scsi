de0a8827ce06a9c4745703ac1fab6467aa768abe
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-01-20 09:20:38 +0100

    parser test: beautifed makefile, added minimum test lattice

--- thor/parser/glps/parser.mk
+++ thor/parser/glps/parser.mk
@@ -1,29 +1,34 @@
 YACC = bison++
 LEX = flexc++
+
 YACC = bison
-YACCFLAGS= -v --defines #="parser.h" # --yacc
-LEX = flex
+YACCFLAGS= -v  --defines -t --yacc #="parser.h" # --yacc
+YACC_OUTPUT=y.tab.c
+# YACC_OUTPUT=parser.tab.c
+
+LEX = flex -d  --posix-compat
+
 
 CC=gcc
-CFLAGS=-Wall -pedantic -c
+CFLAGS=-Wall -pedantic -c -DDEBUG
 LDLIBS=-lm
 
 MV=mv
 
-.PHONY: all clean clean_sr
+.PHONY: all clean clean_src
 
 all : parser_test # parser.o scanner.o
 
-parser.c : parser.tab.c
+parser.c : $(YACC_OUTPUT)
 	$(MV) -f $< $@
 
-parser.tab.c : parser.y
+$(YACC_OUTPUT) : parser.y
 	$(YACC) $(YACCFLAGS) $<
 
 parser_test : parser_test.o parser.o scanner.o
 
 clean : clean_src
-	$(RM) $(RMFLAGS) parser.o scanner.o parser.tab.c parser.c y.tab.c
+	$(RM) $(RMFLAGS) parser.o scanner.o parser_test.o
 
 clean_src :
 	$(RM) $(RMFLAGS) parser.tab.c parser.c y.tab.c scanner.c
